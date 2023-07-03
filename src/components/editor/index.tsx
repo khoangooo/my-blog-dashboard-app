d9a7c5634fbb51523589c5797565db6fad4df632
Khoa Ngo <ngokhoa9596@gmail.com>
2023-07-03 23:45:17 +0700

    feat: use tinymce

--- src/components/editor/index.tsx
+++ src/components/editor/index.tsx
@@ -1,6 +1,5 @@
-import { CKEditor } from 'ckeditor4-react';
-import "./styles.scss";
-import { useEffect } from 'react';
+import { useRef } from 'react';
+import { Editor } from '@tinymce/tinymce-react';
 
 type TProps = {
 	initData?: string;
@@ -9,27 +8,29 @@ type TProps = {
 
 const RichTextEditor = ({ initData = "", onChange = () => { } }: TProps) => {
 
-	const content: string | undefined = initData
+	const content: string | undefined = initData || "<p>This is the initial content of the editor.</p>"
+	const editorRef: any = useRef(null);
 
-	const handleChangeContent = ({ editor }: any) => {
-		onChange(editor.getData());
+	const handleChangeContent = (a: string, editor: Editor) => {
+		onChange(a)
+
 	}
-	const defaultColor = "#00b96b"
+	// const defaultColor = "#00b96b"
 
 	return (
 		<div className='text-editor'>
-			<CKEditor
-				config={{ delayIfDetached: true}}
-				initData={content}
-				// config={{
-				// 	extraPlugins: 'easyimage',
-				// 	removePlugins: 'image',
-				// 	// cloudServices_uploadUrl: 'https://33333.cke-cs.com/easyimage/upload/',
-				// 	// cloudServices_tokenUrl:
-				// 	// 	'https://33333.cke-cs.com/token/dev/ijrDsqFix838Gh3wGO3F77FSW94BwcLXprJ4APSp3XQ26xsUHTi0jcb1hoBt'
-				// }}
-				onChange={handleChangeContent}
-				onInstanceReady={(e) => e.editor.container.setAttribute('style', `--color: ${defaultColor}`)}
+			<Editor
+				apiKey='knuqb65bx38da7iidlz3t8u10dpqty9jvr7kdevvi0hd12sk'
+				onInit={(evt, editor) => editorRef.current = editor}
+				initialValue={content}
+				init={{
+					height: 500,
+					menubar: false,
+					plugins: "'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage  tableofcontents footnotes mergetags autocorrect typography inlinecss",
+					toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+					content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+				}}
+				onEditorChange={handleChangeContent}
 			/>
 		</div>
 	);
